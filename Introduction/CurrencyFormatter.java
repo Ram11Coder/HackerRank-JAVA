da1bbd69be307bc182970cc4a0ae7cf095343558
Ram11Coder <rambeliver11@gmail.com>
2021-11-08 19:34:02 +0530

    Added hacker rank into problems

---
+++ Introduction/CurrencyFormatter.java
@@ -0,0 +1,27 @@
+package introduction;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class CurrencyFormatter {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double payment = scanner.nextDouble();
+		scanner.close();
+
+		String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+		String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+		String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+		String india = String.format("Rs.%,.2f", payment);
+
+		System.out.println("US: " + us);
+		System.out.println("India: " + india);
+		System.out.println("China: " + china);
+		System.out.println("France: " + france);
+
+	}
+}

ced5a02529d3c6d848b07921b884708f3521fbc5
Thulasiraman_97 <64843468+Ram11Coder@users.noreply.github.com>
2022-01-23 20:48:20 +0530

    Created ConstructANumber.java

---
+++ MATHS/Solutions/ConstructANumber.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'canConstruct' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static String canConstruct(List<Integer> a) {
+    // Return "Yes" or "No" denoting whether you can construct the required number.
+int totmax=0;
+for(int i : a){
+    int max=0;
+    while(i>0){
+        max+=i%10;
+        i/=10;
+    }
+    totmax+=(max%3);
+}
+return (totmax%3==0)?"Yes":"No";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                String result = Result.canConstruct(a);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

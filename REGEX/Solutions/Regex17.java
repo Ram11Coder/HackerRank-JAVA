1e989ea5471fd8393dad4ab8375fc3d3d1da31d5
Thulasiraman_97 <64843468+Ram11Coder@users.noreply.github.com>
2022-01-31 23:49:26 +0530

    Updated Regex17

--- REGEX/Solutions/Regex17.java
+++ REGEX/Solutions/Regex17.java
@@ -7,6 +7,8 @@ import java.util.regex.*;
 Parenthesis ( ) around a regular expression can group that part of regex together. This allows us to apply different quantifiers to that group.
 
 These parenthesis also create a numbered capturing. It stores the part of string matched by the part of regex inside parentheses.
+
+(?: ) can be used to create a non-capturing group. It is useful if we do not need the group to capture its match.
 */
 public class Solution {    
 
